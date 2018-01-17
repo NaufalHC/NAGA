f9a18d775cdba590824ac14bf90a308f9b17d549
naufal <naufalzero123@gmail.com>
2018-01-17 12:46:01 +0700

    NAGA

---
+++ app/src/main/java/com/asdf/myfirstgame/Game.java
@@ -0,0 +1,24 @@
+package com.asdf.myfirstgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Turn title off
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Set to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(new GamePanel(this));
+
+
+    }
+}
